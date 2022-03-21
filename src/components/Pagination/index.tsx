e5d83ef159455afe5ac65464866ff50f310aa15b
luiz henrique m silva <luiz.henrique@plannix.com.br>
2022-03-21 11:31:23 -0300

    adding responsiveness

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,62 +1,26 @@
-import { HStack, Button, Box } from "@chakra-ui/react";
+import { Stack, HStack, Button, Box } from "@chakra-ui/react";
+import { PaginationItem } from "./PaginationItem";
 
 export function Pagination() {
   return (
-    <HStack mt="8" justify="space-between" align="center" spacing="6">
+    <Stack
+      direction={["column", "row"]}
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <HStack spacing="2">
-        <Button
-          size="sm"
-          fontSize="xs"
-          colorScheme="pink"
-          w="4"
-          disabled
-          _disabled={{
-            bgColor: "pink.500",
-            cursor: "default",
-          }}
-        >
-          1
-        </Button>
-
-        <Button
-          size="sm"
-          fontSize="xs"
-          w="4"
-          bg="gray.700"
-          _hover={{
-            bg: "gray.500",
-          }}
-        >
-          2
-        </Button>
-
-        <Button
-          size="sm"
-          fontSize="xs"
-          w="4"
-          bg="gray.700"
-          _hover={{
-            bg: "gray.500",
-          }}
-        >
-          3
-        </Button>
-
-        <Button
-          size="sm"
-          fontSize="xs"
-          w="4"
-          bg="gray.700"
-          _hover={{
-            bg: "gray.500",
-          }}
-        >
-          4
-        </Button>
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+        <PaginationItem number={5} />
+        <PaginationItem number={6} />
       </HStack>
-    </HStack>
+    </Stack>
   );
 }
