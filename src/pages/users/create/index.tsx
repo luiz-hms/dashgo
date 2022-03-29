239d993ed735656c00148238a9532032a83520e2
luiz henrique m silva <luiz.henrique@plannix.com.br>
2022-03-29 12:44:30 -0300

    added pagination, data fetching and caching

--- src/pages/users/create/index.tsx
+++ src/pages/users/create/index.tsx
@@ -15,6 +15,10 @@ import { SideBar } from "../../../components/SideBar";
 import { SubmitHandler, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from "react-query";
+import { api } from "../../../services/api";
+import { queryClient } from "../../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -35,13 +39,33 @@ const createUserFormSchema = yup.object().shape({
     .oneOf([null, yup.ref("password")], "As senhas precisam ser iguais"),
 });
 export default function CreateUser() {
+  const router = useRouter();
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema),
   });
   const { errors } = formState;
 
-  const handleCreateUSer: SubmitHandler<CreateUserFormData> = (values) => {
-    console.log(values);
+  const handleCreateUSer: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await createUser.mutateAsync(values);
+    router.push("/users");
   };
   return (
     <Box>
